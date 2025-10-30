36a27911128e9c95995554c1c84ae529e6d362ec
wonke-dev <wonk0311@gmail.com>
2025-10-30 14:53:52 +0900

    zustand 기본 사용법 1

--- onbite-exam/src/App.tsx
+++ onbite-exam/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import IndexPage from "./pages/index-page";
 import SignInPage from "./pages/sign-in-page";
 import SignUpPage from "./pages/sign-up-page";
+import CounterPage from "./pages/counter-page";
 
 function AuthLayout() {
   return (
@@ -18,6 +19,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
+      <Route path="/counter" element={<CounterPage />} />
       <Route element={<AuthLayout />}>
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
