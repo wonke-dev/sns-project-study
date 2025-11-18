6d8ac37a88c99b268ec9d3baf9a4e84e49073749
wonke-dev <wonk0311@gmail.com>
2025-11-18 12:08:34 +0900

    캐시 데이터 다루기 3- 낙관전 업데이트(1)

--- onbite-exam/src/components/todo-list/todo-item.tsx
+++ onbite-exam/src/components/todo-list/todo-item.tsx
@@ -1,21 +1,28 @@
-import { useDeleteTodo } from "@/store/todos";
 import { Button } from "../ui/button";
 import { Link } from "react-router";
+import type { Todo } from "@/types";
+import { useUpdateTodoMutation } from "@/hooks/mutations/use-update-todo-mutation";
 
-export default function TodoItem({
-  id,
-  content,
-}: {
-  id: number;
-  content: string;
-}) {
-  const deleteTodo = useDeleteTodo();
-  const handleDeleteClick = () => {
-    deleteTodo(id);
+export default function TodoItem({ id, content, isDone }: Todo) {
+  const { mutate } = useUpdateTodoMutation();
+  const handleDeleteClick = () => {};
+  const handleCheckboxClick = () => {
+    mutate({
+      id,
+      isDone: !isDone,
+    });
   };
+
   return (
     <div className="flex items-center justify-between border p-2">
-      <Link to={`/todolist/${id}`}>{content}</Link>
+      <div className="flex gap-5">
+        <input
+          onClick={handleCheckboxClick}
+          type={"checkbox"}
+          checked={isDone}
+        />
+        <Link to={`/todolist/${id}`}>{content}</Link>
+      </div>
       <Button onClick={handleDeleteClick} variant={"destructive"}>
         삭제
       </Button>
---
+++ onbite-exam/src/hooks/mutations/use-update-todo-mutation.ts
@@ -0,0 +1,22 @@
+import { updateTodo } from "@/api/update-todo";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export function useUpdateTodoMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateTodo,
+    onMutate: (updateTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [];
+        return prevTodos.map((prevTodo) =>
+          prevTodo.id === updateTodo.id
+            ? { ...prevTodo, ...updateTodo }
+            : prevTodo,
+        );
+      });
+    },
+  });
+}
