ac5c7cefc041746ad408b8e360cd0b9b66a74702
wonke-dev <wonk0311@gmail.com>
2026-01-12 16:44:43 +0900

    라우트 가드 구현하기

--- onebite-log/src/lib/root-route.tsx
+++ onebite-log/src/lib/root-route.tsx
@@ -1,4 +1,6 @@
 import GlobalLayout from "@/components/layout/global-layout";
+import GuestOnlyLayout from "@/components/layout/guest-only-layout";
+import MemberOnlyLayout from "@/components/layout/member-only-layout";
 import ForgetPasswordPage from "@/pages/forget-password-page";
 import IndexPage from "@/pages/index-page";
 import PostDetailPage from "@/pages/post-detail-page";
@@ -12,14 +14,18 @@ export default function RootRoute() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        <Route element={<GuestOnlyLayout />}>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        </Route>
 
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route element={<MemberOnlyLayout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/post/:postId" element={<PostDetailPage />} />
+          <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
         <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
