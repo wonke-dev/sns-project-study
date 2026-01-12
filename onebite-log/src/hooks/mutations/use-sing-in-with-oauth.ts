871e72e495b257c5c7c16504f4b568da0577c480
wonke-dev <wonk0311@gmail.com>
2026-01-12 14:31:19 +0900

    인증 에러 처리하기 2

--- onebite-log/src/hooks/mutations/use-sing-in-with-oauth.ts
+++ onebite-log/src/hooks/mutations/use-sing-in-with-oauth.ts
@@ -1,8 +1,12 @@
 import { singInWithOAuth } from "@/api/auth";
+import type { UseMutationCallback } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
-export function useSignInWithOAuth() {
+export function useSignInWithOAuth(callbacks?: UseMutationCallback) {
   return useMutation({
     mutationFn: singInWithOAuth,
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
   });
 }
--- onebite-log/src/pages/sign-in-page.tsx
+++ onebite-log/src/pages/sign-in-page.tsx
@@ -8,21 +8,34 @@ import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-passwo
 import gitHubLogo from "@/assets/github-mark.svg";
 import { useSignInWithOAuth } from "@/hooks/mutations/use-sing-in-with-oauth";
 import { toast } from "sonner";
+import { generateErrorMessage } from "@/lib/error";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { mutate: signInWithPassword } = useSignInWithPassword({
-    onError: (error) => {
-      toast.error(error.message, {
-        position: "top-center",
-      });
+  const { mutate: signInWithPassword, isPending: isSignInWithPasswordPending } =
+    useSignInWithPassword({
+      onError: (error) => {
+        const message = generateErrorMessage(error);
 
-      setPassword("");
-    },
-  });
-  const { mutate: singInWithOAuth } = useSignInWithOAuth();
+        toast.error(message, {
+          position: "top-center",
+        });
+
+        setPassword("");
+      },
+    });
+  const { mutate: singInWithOAuth, isPending: isSignInWithOAuthPending } =
+    useSignInWithOAuth({
+      onError: (error) => {
+        const message = generateErrorMessage(error);
+
+        toast.error(message, {
+          position: "top-center",
+        });
+      },
+    });
 
   const handleSignInWithPasswordClick = () => {
     if (email.trim() === "") return;
@@ -38,11 +51,14 @@ export default function SignInPage() {
     singInWithOAuth("github");
   };
 
+  const isPending = isSignInWithPasswordPending || isSignInWithOAuthPending;
+
   return (
     <div className="flex flex-col gap-8">
       <div className="text-xl font-bold">로그인</div>
       <div className="flex flex-col gap-2">
         <Input
+          disabled={isPending}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           className="py-6"
@@ -50,6 +66,7 @@ export default function SignInPage() {
           placeholder="example@abc.com"
         />
         <Input
+          disabled={isPending}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="py-6"
@@ -58,10 +75,15 @@ export default function SignInPage() {
         />
       </div>
       <div className="flex flex-col gap-2">
-        <Button onClick={handleSignInWithPasswordClick} className="w-full">
+        <Button
+          disabled={isPending}
+          onClick={handleSignInWithPasswordClick}
+          className="w-full"
+        >
           로그인
         </Button>
         <Button
+          disabled={isPending}
           onClick={handleSignInWithGitHubClick}
           className="w-full"
           variant={"outline"}
